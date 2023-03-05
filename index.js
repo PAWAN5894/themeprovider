9b8f9715ce8fba46396dc2c7fad3fa8717e8724c
PAWAN5894 <121198470+PAWAN5894@users.noreply.github.com>
2023-03-05 13:36:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ThemeContext = React.createContext()
+const ThemeProvider = (props) =>{
+
+    const [state, setState] = useState({
+        theme: "light",
+    });
+    const updateContext = (key, value) => {
+        setState({
+            ...state,
+            [key]: value,
+        });
+    };
+    return (
+        <ThemeContext.Provider value={{ ...state, updateContext }}>
+            {props.children}
+        </ThemeContext.Provider>
+    )
+}
+
+export {ThemeProvider,ThemeContext}
